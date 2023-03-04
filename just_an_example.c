28083628b99579b649af1e5b6f5db2664fce2521
khushiyadav1 <126395244+Meghayadav7@users.noreply.github.com>
2023-03-04 13:33:06 +0530

    new file

---
+++ just_an_example.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main() {
+    printf("hello ");
+    printf("world\n");
+    int a = 1;
+    printf("%d\n",a);
+    a++;
+    printf("%d\n",a);
+    a++;
+    return 0;
+}
