423d0aaf28ef815fac43d5b7951f4c0f5bbc7e2b
khushiyadav1 <126395244+Meghayadav7@users.noreply.github.com>
2023-03-20 15:53:56 +0530

    new file

---
+++ function_eg.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+void goodmorning();
+void goodafternoon();
+void goodnight();
+
+int main() {
+    goodmorning();
+    goodafternoon();
+    goodnight();
+    return 0;
+}
+void goodmorning(){
+    printf("Goodmorning Megha\n");
+} 
+void goodafternoon(){
+    printf("Goodafternoon Megha\n");
+}
+void goodnight(){
+    printf("Goodnight Megha\n");
+}
